9e27e509257c45a6eedeee7287bfc497c83b8f3f
Hector Castro <hectcastro@gmail.com>
2013-04-25 16:10:50 -0400

    Added conditionals around Riak CS Control specific resources and notifications so that they don't get created if Control's recipe isn't in the run list.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,10 @@ ruby_block "trigger-delayed-restarts" do
   notifies :restart, "service[riak]", :immediately
   notifies :restart, "service[stanchion]", :immediately
   notifies :restart, "service[riak-cs]", :immediately
-  notifies :restart, "service[riak-cs-control]", :immediately
+
+  if node.recipe?("riak-cs::control")
+    notifies :restart, "service[riak-cs-control]", :immediately
+  end
 end
 
 ruby_block "create-admin-user" do
@@ -38,15 +41,17 @@ ruby_block "create-admin-user" do
     stanchion_file = resources(:file => "#{node['stanchion']['package']['config_dir']}/app.config")
     stanchion_file.content Eth::Config.new(stanchion_config).pp
 
-    riak_cs_control_config = node["riak_cs_control"]["config"].to_hash
-    riak_cs_control_config = riak_cs_control_config.merge(
-      "riak_cs_control" => riak_cs_control_config["riak_cs_control"].merge(
-        "cs_admin_key" => admin_key.to_erl_string,
-        "cs_admin_secret" => admin_secret.to_erl_string
-      )
-    )
-    riak_cs_control_file = resources(:file => "#{node['riak_cs_control']['package']['config_dir']}/app.config")
-    riak_cs_control_file.content Eth::Config.new(riak_cs_control_config).pp
+    if node.recipe?("riak-cs::control")
+      riak_cs_control_config = node["riak_cs_control"]["config"].to_hash
+      riak_cs_control_config = riak_cs_control_config.merge(
+        "riak_cs_control" => riak_cs_control_config["riak_cs_control"].merge(
+          "cs_admin_key" => admin_key.to_erl_string,
+          "cs_admin_secret" => admin_secret.to_erl_string
+          )
+        )
+      riak_cs_control_file = resources(:file => "#{node['riak_cs_control']['package']['config_dir']}/app.config")
+      riak_cs_control_file.content Eth::Config.new(riak_cs_control_config).pp
+    end
 
     node.set["riak_cs"]["config"]["riak_cs"]["anonymous_user_creation"] = false
   end
@@ -55,6 +60,10 @@ ruby_block "create-admin-user" do
   retry_delay 3
   notifies :create, "file[#{node['stanchion']['package']['config_dir']}/app.config]", :immediately
   notifies :create, "file[#{node['riak_cs']['package']['config_dir']}/app.config]", :immediately
-  notifies :create, "file[#{node['riak_cs_control']['package']['config_dir']}/app.config]", :immediately
+
+  if node.recipe?("riak-cs::control")
+    notifies :create, "file[#{node['riak_cs_control']['package']['config_dir']}/app.config]", :immediately
+  end
+
   only_if { node["riak_cs"]["config"]["riak_cs"]["anonymous_user_creation"] }
 end
